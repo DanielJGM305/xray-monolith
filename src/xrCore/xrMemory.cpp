81ce02fd47712f8d24ec23628a154f1244adc3ac
Daniel Gómez <dmhola102@gmail.com>
2026-04-25 23:30:56 -0400

    fix: mem_compact() ahora funciona en Release, no solo en Debug

--- src/xrCore/xrMemory.cpp
+++ src/xrCore/xrMemory.cpp
@@ -145,16 +145,17 @@ void xrMemory::_destroy()
 
 void xrMemory::mem_compact()
 {
+    if (g_allow_heap_min)
+        _heapmin();
+    HeapCompact(GetProcessHeap(), 0);
+    if (g_pStringContainer) g_pStringContainer->clean();
+    if (g_pSharedMemoryContainer) g_pSharedMemoryContainer->clean();
+    if (strstr(Core.Params, "-swap_on_compact"))
+        SetProcessWorkingSetSize(GetCurrentProcess(), size_t(-1), size_t(-1));
+
 #ifdef DEBUG_MEMORY_MANAGER
-	RegFlushKey(HKEY_CLASSES_ROOT);
-	RegFlushKey(HKEY_CURRENT_USER);
-	if (g_allow_heap_min)
-		_heapmin();
-	HeapCompact(GetProcessHeap(), 0);
-	if (g_pStringContainer) g_pStringContainer->clean();
-	if (g_pSharedMemoryContainer) g_pSharedMemoryContainer->clean();
-	if (strstr(Core.Params, "-swap_on_compact"))
-		SetProcessWorkingSetSize(GetCurrentProcess(), size_t(-1), size_t(-1));
+    RegFlushKey(HKEY_CLASSES_ROOT);
+    RegFlushKey(HKEY_CURRENT_USER);
 #endif
 }
 
